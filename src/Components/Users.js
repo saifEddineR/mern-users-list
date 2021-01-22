65e13b0d85b688de1689083581a79d595ed20ddd
saif eddine romdhane <32793306+saifEddineR@users.noreply.github.com>
2021-01-22 16:27:17 +0100

    :smile:

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import UserCard from './UserCard';
+import axios from 'axios';
+
 const Users = () => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    axios
+      .get('/user')
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <UserCard />
